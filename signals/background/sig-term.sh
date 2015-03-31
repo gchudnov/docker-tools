cf2558fc70e19c206e24add0a2b9c5fc0ad5c13c
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-03-31 18:07:48 +0300

    added stop comment

--- signals/background/sig-term.sh
+++ signals/background/sig-term.sh
@@ -3,3 +3,6 @@ set -ex
 
 # gracefully stop the app
 docker kill --signal="SIGTERM" signal-bg-app
+
+# or
+# docker stop signal-fg-app
--- signals/foreground/sig-term.sh
+++ signals/foreground/sig-term.sh
@@ -3,3 +3,6 @@ set -ex
 
 # gracefully stop the app
 docker kill --signal="SIGTERM" signal-fg-app
+
+# or
+# docker stop signal-fg-app
